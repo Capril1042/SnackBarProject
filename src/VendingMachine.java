b83232badc0f49f7d8728d45c42d9eaf1acd51b2
Capril1042 <Caprilewis1042@gmail.com>
2019-02-04 15:24:49 -0500

    VendingMachine class

---
+++ src/VendingMachine.java
@@ -0,0 +1,22 @@
+package snackbarApp;
+
+public class VendingMachine
+{
+    private static id;
+    public String name;
+
+    public VendingMachine (String name)
+    {
+	this.name = name;
+    }
+
+    public String getName()
+    {
+	return name;
+    }
+
+    public int setName(String name)
+    {
+	this.name = name;
+    }
+}
